fdad161c1383f5416b7f2e8f229cad6ea2923eaa
Srikanth Vudharapu <sidharthsid74659@gmail.com>
2021-08-13 20:03:40 +0530

    Update 02_03_pointer_problems.cpp

--- 02_03_pointer_problems.cpp
+++ 02_03_pointer_problems.cpp
@@ -28,11 +28,38 @@ int main()
         ii.  Memory Leak
 
             CAUSE : when you don't free/ deallocate memory in HEAP, it will eventually run out of memory, which causes memory leak.
-                This happens either when you don't free/ deallocate memory or when you point the pointer to null || NULL || nullptr before deleting
+                This happens either when you don't free/ deallocate memory or when you point the pointer to null || NULL || 0 || nullptr (suggested, it is a reserved word(keyword), it's a zero but not a integer value but address value); before deleting
 
+            SOLUTION :
+                a. delete the pointer(de-allocate) when no longer in use :
+                    delete []ptr;
+                b. and assign it to null ONLY when you already deleted using above syntax
+                    p = null || NULL || 0 || nullptr (suggested);
             
         iii. Dangling Pointer
-    
+
+            CAUSE : when you have two pointers sharing same memory and you deleted one pointer, the other pointer is left to pointed something that doesn't exist, this causes error.
+                In simple words, accessing the memory that is already deallocated
+
+                void fun(int *q) {
+                    .........
+                    delete []q; 
+                }
+
+                void main () {
+                    int *p = new int[5];
+                    .......
+                    fun(p); //deletes the pointer p is pointing to
+                    cout << *p; //causes error as now 'p' points to something that doesn't exist
+                }
+
+            
+            SOLUTION : 
+                a. write code carefully
+                b. concentrate on possible errors while running
+
+
+    Cause of this such errors, JAVA and DOTNET removed pointers however they handle all this (like de-allocating..) inside JVM (another reason why java is easier)
     
     */
 
